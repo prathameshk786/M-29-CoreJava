e7f726afb6c5d750e890d73e47b4571931535449
2144AX <2144AX@Prathamesh>
2023-06-12 10:39:03 +0530

    Commit

--- daynine/src/org/tnsif/arrays/MultiDimesionalArray.java
+++ daynine/src/org/tnsif/arrays/MultiDimesionalArray.java
@@ -27,13 +27,12 @@ public class MultiDimesionalArray {
 class Jaggedarray{
 	//Dec
 	int arr[][]= new int[2][];
-	.
 	arr[0]=new int[3];
 	arr[1]=new int[2];
 	int count=0;
 	for(int i=0; i<arr.length;i++)
 		for(int j=0; j<arr[i].length;j++)
 			arr[i][j]=count++;
-	System.out.println()
+	System.out.println();
 }
 }
